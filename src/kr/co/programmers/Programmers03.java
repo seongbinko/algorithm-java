058baf4813b7849358d7c7af19d3ab4e84ba5170
SEONGBIN KO <wnrhd1082@gmail.com>
2020-04-08 18:52:51 +0900

    아무소문자 대문자로 바꾸기 ,자연수 뒤집기

---
+++ src/kr/co/programmers/Programmers03.java
@@ -0,0 +1,41 @@
+package kr.co.programmers;
+
+public class Programmers03 {
+    /*
+    * 문제 설명
+        문자열 s는 한 개 이상의 단어로 구성되어 있습니다. 각 단어는 하나 이상의 공백문자로 구분되어 있습니다.
+        각 단어의 짝수번째 알파벳은 대문자로, 홀수번째 알파벳은 소문자로 바꾼 문자열을 리턴하는 함수, solution을 완성하세요.
+      제한 사항
+        문자열 전체의 짝/홀수 인덱스가 아니라, 단어(공백을 기준)별로 짝/홀수 인덱스를 판단해야합니다.
+        첫 번째 글자는 0번째 인덱스로 보아 짝수번째 알파벳으로 처리해야 합니다.
+      입출력 예
+        try hello world는 세 단어 try, hello, world로 구성되어 있습니다.
+        각 단어의 짝수번째 문자를 대문자로, 홀수번째 문자를 소문자로 바꾸면 TrY, HeLlO, WoRlD입니다. 따라서 TrY HeLlO WoRlD 를 리턴합니다.
+    * */
+    public static void main(String[] args) {
+        String s = "try hello world";
+        String answer = solution(s);
+
+        System.out.println(answer);
+    }
+    public static String solution(String s) {
+        StringBuilder sb = new StringBuilder();
+        int index = 0;
+
+        for(int i=0; i<s.length(); i++) {
+            if(s.charAt(i) == ' ') {
+                sb.append(" ");
+                index = 0;
+            } else {
+                if(index%2 !=0) {
+                    sb.append(s.charAt(i));
+                    index ++;
+                } else {
+                    sb.append(Character.toUpperCase(s.charAt(i)));
+                    index ++;
+                }
+            }
+        }
+        return sb.toString();
+    }
+}
---
+++ src/kr/co/programmers/Programmers04.java
@@ -0,0 +1,44 @@
+package kr.co.programmers;
+/*
+* 자연수 n을 뒤집어 각 자리 숫자를 원소로 가지는 배열 형태로 리턴해주세요. 예를들어 n이 12345이면 [5,4,3,2,1]을 리턴합니다.
+* */
+public class Programmers04 {
+    public static void main(String[] args) {
+        long n = 123456789103L;
+        //solution01(n);
+        solution02(n);
+    }
+
+    public static void solution01(long n) {
+        String str = String.valueOf(n);
+
+        String[] arr = str.split("");
+
+        int[] answer = new int[arr.length];
+
+        int j = 0;
+        for (int i = arr.length - 1; i >= 0; i--) {
+            answer[j] = Integer.parseInt(arr[i]);
+            j++;
+        }
+
+        for (int i : answer) System.out.println(i);
+    }
+    public  static  void solution02(long n) {
+        String str = n + "";
+        int[] answer = new int[str.length()];
+
+        StringBuilder sb = new StringBuilder(str);
+        sb.reverse();
+
+        String[] text = sb.toString().split("");
+
+        for(int i=0; i<answer.length; i ++) {
+            answer[i] = Integer.parseInt(text[i]);
+            System.out.println(answer[i]);
+        }
+    }
+}
+/*
+*
+* */
