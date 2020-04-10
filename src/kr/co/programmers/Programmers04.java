0edbef6ec9d00640bf692c2ab2b102f650b8eb70
SEONGBIN KO <wnrhd1082@gmail.com>
2020-04-10 22:07:27 +0900

    04 import Arrays 07 같은 숫자는 싫어

--- src/kr/co/programmers/Programmers04.java
+++ src/kr/co/programmers/Programmers04.java
@@ -1,4 +1,7 @@
 package kr.co.programmers;
+
+import java.util.Arrays;
+
 /*
 * 자연수 n을 뒤집어 각 자리 숫자를 원소로 가지는 배열 형태로 리턴해주세요. 예를들어 n이 12345이면 [5,4,3,2,1]을 리턴합니다.
 * */
@@ -33,12 +36,9 @@ public class Programmers04 {
 
         String[] text = sb.toString().split("");
 
-        for(int i=0; i<answer.length; i ++) {
+        for (int i = 0; i < answer.length; i++) {
             answer[i] = Integer.parseInt(text[i]);
-            System.out.println(answer[i]);
         }
+        System.out.println(Arrays.toString(answer));
     }
 }
-/*
-*
-* */
