5b5e066958feb649a2ab2f5b4a84fe0fb611701a
SEONGBIN KO <wnrhd1082@gmail.com>
2020-04-14 14:51:52 +0900

    Programmers11. java

---
+++ src/kr/co/programmers/Programmers11.java
@@ -0,0 +1,24 @@
+package kr.co.programmers;
+
+public class Programmers11 {
+    public static void main(String[] args) {
+        int a=1111123;
+        int b=12243234;
+        solution(a,b);
+    }
+    public static void solution(int a, int b) {
+
+        long answer = ;
+
+        if(a == b) {
+            answer = a;
+        }
+        if(a-b >0) {
+            answer = a*(a+1)/2 - (b-1)*(b)/2;
+        } else {
+            answer = b*(b+1)/2 - (a-1)*(a)/2;
+        }
+
+        System.out.println(answer);
+    }
+}
