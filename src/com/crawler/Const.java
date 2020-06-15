8aeed94875a74de25f0dcfa9808fcabd172a6f8a
seongbinko <wnrhd1082@gmail.com>
2020-06-16 00:23:39 +0900

    Crawling Test

---
+++ src/com/crawler/Const.java
@@ -0,0 +1,10 @@
+package com.crawler;
+
+public class Const {
+
+    public final static int DEFAULT_THREADS = 2;
+    public final static String DEFAULT_DOWNSLOADS = "C:/test123/";
+    public final static String DEFAULT_USER_AGENT = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.97 Safari/537.36";
+    public final static String DEFAULT_CONTENT_TYPE = "application/json;charset=UTF-8";
+    public final static int DEFAULT_TIMEOUT = 5*1000;
+}
---
+++ src/com/crawler/Crawler.java
@@ -0,0 +1,43 @@
+package com.crawler;
+
+import org.jsoup.Connection;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+public class Crawler {
+    public static void main(String[] args) {
+        try {
+            String URL = "https://www.10000recipe.com/recipe/6924266";
+
+            Connection conn = Jsoup.connect(URL);
+
+            Document html = conn.get();
+
+            System.out.println("\n[CSS Slector 탐색]");
+            String userId = html.select(".cont_name").text();
+            String recipeName = html.select(".view2_summary h3").text();
+            String src = html.getElementById("main_thumbs").attr("src");
+            Elements stepInfos = html.select("div[id^='stepDiv']");
+
+            System.out.println("레시피 작성자 >> " + userId);
+            System.out.println("레시피 제목 >> " + recipeName);
+            System.out.println("레시피 대표사진 >> " + src);
+
+            System.out.println("스텝 사이즈 >> " + stepInfos.size());
+            for (Element stepInfo : stepInfos) {
+
+                System.out.println(stepInfo.text());
+                if(!stepInfo.select("img").attr("src").isEmpty()) {
+                    System.out.println(stepInfo.select("img").attr("src"));
+                } else {
+                    System.out.println("null");
+                }
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
