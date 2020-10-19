2e395348fcd972bc5aa56e4327daf5cd9bdb0e3b
seongbinko <wnrhd1082@gmail.com>
2020-10-19 15:52:54 +0900

    완주하지 못한 선수 새로운 솔루션3 추가 (실패)

--- src/kr/co/programmers/Programmers10.java
+++ src/kr/co/programmers/Programmers10.java
@@ -1,6 +1,7 @@
 package kr.co.programmers;
 
 import java.util.Arrays;
+import java.util.HashMap;
 //완주하지 못한선수 hash
 public class Programmers10 {
     //2차
@@ -8,8 +9,10 @@ public class Programmers10 {
         String[] participant = {"marina", "josipa", "nikola", "vinko", "filipa"};
         String[] completion = {"josipa", "filipa", "marina", "nikola"};
 
-        System.out.println(solution(participant,completion));
-        System.out.println(solution2(participant,completion));
+        System.out.println(solution(participant, completion));
+        System.out.println(solution2(participant, completion));
+        // 10월 19일 추가 테스트 통과 하지 못함
+        System.out.println(solution3(participant, completion));
     }
     public static String solution(String[] participant, String[] completion) {
         Arrays.sort(participant);
@@ -28,16 +31,38 @@ public class Programmers10 {
     public static String solution2(String[] participant, String[] completion) {
         Arrays.sort(participant);
         Arrays.sort(completion);
-        String[] comp = Arrays.copyOf(completion,completion.length+1);
+        String[] comp = Arrays.copyOf(completion, completion.length + 1);
         comp[completion.length] = "";
 
         String answer = "";
 
-        for (int i=0; i<participant.length; i++){
-            if(!comp[i].equals(participant[i])) {
+        for (int i = 0; i < participant.length; i++) {
+            if (!comp[i].equals(participant[i])) {
                 answer = participant[i];
                 break;
             }
+        }
+        return answer;
+    }
+
+    public static String solution3(String[] participant, String[] completion) {
+        String answer = "";
+
+        HashMap<Integer, String> hmap = new HashMap<>(participant.length);
+        for (int i = 0; i < participant.length; i++) {
+            hmap.put(i, participant[i]);
+        }
+
+        for (int i = 0; i < completion.length; i++) {
+            for (int j = 0; j < participant.length; j++) {
+                if (completion[i].equals(participant[j])) {
+                    hmap.remove(j);
+                    break;
+                }
+            }
+        }
+        for (Integer key : hmap.keySet()) {
+            answer = hmap.get(key);
         }
         return answer;
     }
