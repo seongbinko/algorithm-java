9809eec3cb8a40abd596a08a5270be7f626b4be0
seongbinko <wnrhd1082@gmail.com>
2021-11-14 17:49:54 +0900

    add [프로그래머스] 게임 맵 최단거리(bfs) level2

--- src/com/programmers/게임맵_최단거리.java
+++ src/com/programmers/게임맵_최단거리.java
@@ -3,89 +3,91 @@ package com.programmers;
 import java.util.LinkedList;
 import java.util.Queue;
 
+/**
+ * https://programmers.co.kr/learn/courses/30/lessons/1844?language=java
+ * 방문 처리를 해버리면 다른 루트에서 접근할 때 도착하지를 못한다. bfs일 경우 최단 거리로 갈 수 있기 때문에 방문처리를 false로 바꿀 일이 필요 없다.
+ */
 public class 게임맵_최단거리 {
 
     public static void main(String[] args) {
         int[][] maps = {
-                {1,0,1,1,1},
-                {1,0,1,0,1},
-                {1,0,1,1,1},
-                {1,1,1,0,1},
-                {0,0,0,0,1}
-        };
-        // 11
+                {1, 0, 1, 1, 1},
+                {1, 0, 1, 0, 1},
+                {1, 0, 1, 1, 1},
+                {1, 1, 1, 0, 1},
+                {0, 0, 0, 0, 1}
+        }; // 11
         int[][] maps2 = {
                 {1, 0, 1, 1, 1},
                 {1, 0, 1, 0, 1},
-                {1,0,1,1,1},
-                {1,1,1,0,0},
-                {0,0,0,0,1}
+                {1, 0, 1, 1, 1},
+                {1, 1, 1, 0, 0},
+                {0, 0, 0, 0, 1}
         }; // -1
-
-
 
         게임맵_최단거리 게임맵최단거리 = new 게임맵_최단거리();
         System.out.println(게임맵최단거리.solve_bfs(maps2));
     }
 
+    int[][] dirs = {
+            {-1, 0},
+            {1, 0},
+            {0, -1},
+            {0, 1}
+    };
+
+    int m, n;
+    boolean[][] visit;
 
     private int solve_bfs(int[][] maps) {
-        int min = Integer.MAX_VALUE;
-
-        int n = maps.length;
-        int m = maps[0].length;
-
-        int[][] dirs = {{-1,0},{1,0},{0,-1},{0,1}};
-
-        int[] start = {0,0};
-        int[] dest = {n - 1, m - 1};
-
-        if (start[0] == dest[0] && start[1] == dest[1]) {
+        // 1. 방향 설정 및 2차원 배열 사이즈
+        if (maps == null || maps.length < 1 || maps[0].length < 1) {
             return -1;
         }
-
-        boolean[][] visited = new boolean[n][m];
-        visited[start[0]][start[1]] = true; // start point
-
-        Queue<int[]> q = new LinkedList<>();
-        q.offer(new int[]{start[0], start[1]});
-
-        int count = 1;
-        while (!q.isEmpty()) {
-            int[] p = q.poll();// 0, 0
-            for (int[] dir : dirs) {
-                int x = p[0];
-                int y = p[1];
-
-                while (x >= 0 && x < n && y >= 0 && y < m && maps[x][y] == 1) {
-                    visited[x][y] = true;
-                    count ++;
-                    x += dir[0];
-                    y += dir[1];
-                    System.out.println("x = " + x +", y = " + y);
-                }
-                x -= dir[0];
-                y -= dir[1];
-
-//                if (visited[x][y]) {
-//                    continue;
-//                }
-//                visited[x][y] = true;
-//                count++;
-                System.out.println("=================");
-
-                if(x == dest[0] && y == dest[1]) {
-                    min = Math.min(min, count);
-                    count = 1;
-                }
-
-                q.offer(new int[]{x, y});
-            }
+        if (maps.length == 1 && maps[0].length == 1) {
+            return -1;
         }
-        if(visited[dest[0]][dest[1]]) {
-            return min;
+        m = maps.length; // maps[m-1][n-1]이 최종 도착해야 할 좌표
+        n = maps[0].length;
+
+        visit = new boolean[m][n];
+
+        return bfs(maps, 0 , 0); // 시작점이 정해져 있지 않으니까?
+    }
+
+    private int bfs(int[][] maps, int x, int y) {
+
+        Queue<Node> q = new LinkedList<>();
+        q.offer(new Node(x, y, 1));
+        visit[x][y] = true; // 방문 처리
+
+        while (!q.isEmpty()) {
+            Node node = q.poll();
+            if (node.x == m - 1 && node.y == n - 1) {
+                return node.cost;
+            }
+            for (int[] dir : dirs) {
+                int vx = node.x + dir[0]; // vx
+                int vy = node.y + dir[1]; // vy
+
+                if (vx >= 0 && vx < m && vy >= 0 && vy < n && maps[vx][vy] == 1 && !visit[vx][vy]) {
+                    visit[vx][vy] = true;
+                    q.offer(new Node(vx, vy, node.cost + 1));
+                }
+            }
         }
         return -1;
     }
 
+    private class Node {
+        int x;
+        int y;
+        int cost;
+
+        private Node(int x, int y, int cost) {
+            this.x = x;
+            this.y = y;
+            this.cost = cost;
+        }
+    }
 }
